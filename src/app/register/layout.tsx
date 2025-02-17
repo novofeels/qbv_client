2b2ed36c4a2c8fdd785100d8ac12c28be7819689
matt novotny <mattrnovotny@gmail.com>
2025-02-17 14:32:42 -0600

    register page

--- src/app/register/layout.tsx
+++ src/app/register/layout.tsx
@@ -9,11 +9,9 @@ export default function RegisterLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <header className="p-4 bg-blue-600 text-white">
-        <h1>Register Your Account</h1>
-      </header>
-      <main className="p-6">{children}</main>
-    </div>
+
+
+      <main className="">{children}</main>
+
   );
 }
