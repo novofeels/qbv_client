d7c81088b728ab2ded365c14c246acefb3954617
matt novotny <mattrnovotny@gmail.com>
2025-02-17 16:13:30 -0600

    latest changes to pull onto mac

--- src/app/approval/layout.tsx
+++ src/app/approval/layout.tsx
@@ -1,4 +1,4 @@
-// src/app/approval/layout.tsx
+// src/app/register/layout.tsx
 'use client';
 
 import React from 'react';
@@ -9,11 +9,9 @@ export default function ApprovalLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-white">
-      <header className="p-4 bg-green-600 text-white">
-        <h1>Review Your Information</h1>
-      </header>
-      <main className="p-6">{children}</main>
-    </div>
+
+
+      <main className="">{children}</main>
+
   );
 }
