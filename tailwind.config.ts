f646018917b8bf5c00ab5f3e7a2fc124eb25133f
matt novotny <mattrnovotny@gmail.com>
2025-02-17 09:29:34 -0600

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,17 @@
-import type { Config } from "tailwindcss";
+import daisyui from 'daisyui';
+import type { Config } from 'tailwindcss';
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["business", "winter","light"],
   },
-  plugins: [],
-} satisfies Config;
+};
+
+export default config;
