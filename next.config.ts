59db1e68ee719398712af89926c72eb954f32506
matt novotny <mattrnovotny@gmail.com>
2025-03-05 11:51:18 -0600

    commit

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/qbv_client',
   assetPrefix: '/qbv_client/',
   trailingSlash: true, // This forces Next.js to output directories with trailing slashes
   images: {
