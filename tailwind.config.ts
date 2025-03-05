198fa8c893903a72f8ed84b9cf5e68dea6303c38
matt novotny <mattrnovotny@gmail.com>
2025-03-05 11:19:16 -0600

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,8 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/*.{js,ts,jsx,tsx,mdx}"
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
 
   plugins: [daisyui],
